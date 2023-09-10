a8225769a3c0fda0a6bfe674dd8039ab2e032aab
Taijul Islam <taijulislam.st9@gmail.com>
2023-09-10 15:52:22 +0600

    string concatation

--- main.dart
+++ main.dart
@@ -1,17 +1,23 @@
 void main() {
-  String connection = "connected";
+  String a = "We";
+  String b = "Are";
+  String c = "Bangladeshi";
+  String d = "Fighter";
 
-  if (connection == "connected") {
-    print('Connected');
-  } else {
-    print("Not Connected");
-  }
+  // String Literals
 
-  switch (connection) {
-    case "connected":
-      print("Connected");
-      break;
-    default:
-      print("disconnected");
-  }
+  String e = "We" "Are" "Bangladeshi" "Fighter";
+
+  //1st step
+  print(a + b + c + d);
+
+  // 2nd step
+  print("$a$b$c$d");
+  //3rd step
+  print(e);
+
+  //4th step
+  List<String> f = ["We", "Are", "Bangladeshi", "Fighter"];
+
+  print(f.join(","));
 }
